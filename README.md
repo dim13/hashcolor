cbc39dea9177a4f860208f429b25c91b70df0fb1
Dimitri Sokolyuk <demon@dim13.org>
2019-08-09 12:30:01 +0200

    convert to package

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # hashcolor
 
 Simple color randomizer for uxterm
+
+## install
+
+	go get -u github.com/dim13/hashcolor/cmd/hashcolor
--- hashcolor.go
+++ hashcolor.go
@@ -1,15 +1,14 @@
-package main
+package hashcolor
 
 import (
-	"fmt"
 	"image/color"
-	"os"
-	"strings"
 
 	"github.com/dim13/crc24"
 )
 
-func toColor(c uint32) color.RGBA {
+// New color from hashed string
+func New(s string) color.RGBA {
+	c := crc24.Sum([]byte(s))
 	return color.RGBA{
 		R: uint8(c >> 0x10),
 		G: uint8(c >> 0x08),
@@ -18,12 +17,8 @@ func toColor(c uint32) color.RGBA {
 	}
 }
 
-func hex(c color.Color) string {
-	rgba := color.RGBAModel.Convert(c).(color.RGBA)
-	return fmt.Sprintf("#%.2x%.2x%.2x", rgba.R, rgba.G, rgba.B)
-}
-
-func tint(c color.Color) color.RGBA {
+// Tint color
+func Tint(c color.Color) color.RGBA {
 	rgba := color.RGBAModel.Convert(c).(color.RGBA)
 	rgba.R += (0xff - rgba.R) >> 1
 	rgba.G += (0xff - rgba.G) >> 1
@@ -31,16 +26,11 @@ func tint(c color.Color) color.RGBA {
 	return rgba
 }
 
-func shade(c color.Color) color.RGBA {
+// Shade color
+func Shade(c color.Color) color.RGBA {
 	rgba := color.RGBAModel.Convert(c).(color.RGBA)
 	rgba.R >>= 2
 	rgba.G >>= 2
 	rgba.B >>= 2
 	return rgba
-}
-
-func main() {
-	arg := strings.Join(os.Args[1:], " ")
-	c := toColor(crc24.Sum([]byte(arg)))
-	fmt.Println("-fg", hex(tint(c)), "-bg", hex(shade(c)))
 }
